b308a7e494c99d1e16c780cdf68f4af66c0cff5a
Karla Polo <karla.polo-garcia@capco.com>
2019-02-13 17:40:12 -0500

    Class4

---
+++ Class4/Starter Code /js/main.js
@@ -0,0 +1,22 @@
+/*
+ * Original Code by Lucien Huang! 
+ */
+
+//This is the array of the images
+var images = [
+    "newschoolbuilding.jpg",
+    "050615_Festival_052_750x563.jpg",
+    "745127-original.jpg",
+    "1496605813404.jpeg",
+    "bigimg_parsons.png",
+    "D10_01.jpg",
+    "DnKbS0IX4AEBA5-.jpg",
+    "exterior-PortalImages.jpg",
+    "newschoolbuilding.jpg",
+    "Parsons_about_5607thave.jpg",
+    "Parsons-about.jpg",
+    "SP16_SummerIntensive_Nector_Parsons.jpg",
+    "The-New-School-Student-Life-3.jpg",
+    "The-New-School’s-Parsons-School-of-Design-3.jpg",
+    "tish.jpg"];
+
